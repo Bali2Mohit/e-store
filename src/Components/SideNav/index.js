ef8ef2db32703c21293427a017cc5acaf29a2c13
Mohit Bali <bali2mohit@gmail.com>
2023-08-27 15:09:07 +0100

    Itegrated Redux and Backend

--- src/Components/SideNav/index.js
+++ src/Components/SideNav/index.js
@@ -1,32 +1,72 @@
+import { useSelector, useDispatch } from 'react-redux';
 import './_side-nav.scss';
+import AccordionSlice from '../../Redux/Accordion/accordionSlice';
+import { useEffect } from 'react';
+import {getCategories} from '../../Redux/Category/actions';
 
 const SideNav = () => {
-    return (
+
+  const accordionData = useSelector(state=>state.categoryReducer.categories);
+  console.log(accordionData);
+  const dispatch = useDispatch();
+  useEffect((()=>{
+    dispatch(getCategories)
+  }),[]);
+
+  return (
       <div className="side-nav">
         <div className="section-title">
-          <h3>Catogery</h3>
+          <h3>Category</h3>
         </div>
 
         <div className="accordion">
-          <div className="accordion-item individual-category">
-            <div className="accordion-header">
-              <button className="accordion-button" data-bs-target='#accordian-heading-one' data-bs-toggle='collapse'>
-                <div className="category-title">
-                  <a href="#">Men</a>
-                </div>
-              </button>
-            </div>
-            <div className="accordion-collapse collapse show" id='accordian-heading-one'>
-              <button className="accordion-body">
-                <ul> 
-                  <li className='sub-items'><a href='#'>Coats</a></li>
-                  <li className='sub-items'><a href='#'>Boots</a></li>
-                  <li className='sub-items'><a href='#'>Party Wear</a></li>
-                  <li className='sub-items'><a href='#'>Shirts</a></li>
-                </ul>
-              </button>
-            </div>
-          </div>
+          {
+            accordionData.map((accordionCategory,key)=>{
+              if (
+                accordionCategory.parent_category_id === null &&
+                accordionCategory.category !== 'All' && accordionCategory.category !== 'Best Offers')
+               {
+                return (
+                  <div className="accordion-item individual-category">
+                    <div className="accordion-header">
+                      <button
+                        className="accordion-button"
+                        data-bs-target={"#collapse" + key}
+                        data-bs-toggle="collapse"
+                      >
+                        <div className="category-title">
+                          <a href="#">{accordionCategory.category}</a>
+                        </div>
+                      </button>
+                    </div>
+                    <div
+                      className="accordion-collapse collapse show"
+                      id={"collapse" + key}
+                    >
+                      <button className="accordion-body">
+                        <ul>
+                          {accordionData.map((subCategory) => {
+                            if (
+                              accordionCategory.id ===
+                              subCategory.parent_category_id
+                            ) {
+                              return (
+                                <li className="sub-items">
+                                  <a href="#">{subCategory.category}</a>
+                                </li>
+                              );
+                            }
+                          })}
+                        </ul>
+                      </button>
+                    </div>
+                  </div>
+                );
+              }
+              
+            })  
+          }
+          
         </div>
       </div>
     );
